5d6c265cb181aab73cf1328c10dad75bb59a0270
lyjelin <u3560393@connect.hku.hk>
2021-06-20 16:35:19 +0800

    Loading Screen

--- App.js
+++ App.js
@@ -1,34 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading.js";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>Yellow View Here</Text>
-      </View>
-      <View style={styles.blueview}>
-        <Text>Blue VIew Here</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //backgroundColor: '#fff',
-    //alignItems: 'center',
-    justifyContent: 'center',
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor:"yellow"
-  },
-  blueview: {
-    flex: 1,
-    backgroundColor:"blue"
-  },
-});
