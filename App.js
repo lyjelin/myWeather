2d2b5dbde82d786b970e5590e3960e1374ea71f0
lyjelin <u3560393@connect.hku.hk>
2021-06-22 15:22:39 +0800

    Title and subtitle

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ export default class extends React.Component {
         coords: { latitude, longitude } 
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      
+      this.setState({ isLoading: false })
     } catch (error){
       Alert.alert("Can't find you.", "So sad");
     }
@@ -46,4 +46,4 @@ export default class extends React.Component {
     const { isLoading, temp, condition } = this.state;
     return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
-}
+};
